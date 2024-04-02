8483d931a7c15be8c93257582a647a14ef486a96
Shiv Modi <modishiv210@gmail.com>
2024-04-02 14:22:47 +0530

    Week 8 Added

--- Sparta/Week 8/notebook/static/script.js
+++ Sparta/Week 8/notebook/static/script.js
@@ -7,11 +7,10 @@ $(document).ready(function() {
         event.preventDefault();
         var noteTitle = $('#noteTitle').val().trim();
         var noteText = $('#noteText').val().trim();
-        if(noteText === ""){
+        if (noteText === "") {
             alert('Note is empty!')
-        }
-        else{
-            if(noteTitle === ""){
+        } else {
+            if (noteTitle === "") {
                 noteTitle = "Untitled"
             }
             addNote(noteTitle, noteText);
@@ -26,13 +25,14 @@ $(document).ready(function() {
             success: function(data) {
                 $('#noteList').empty();
                 data.forEach(function(note) {
-                    temp_html = `
-                    <li class="list-group-item">
-                    <h3>${note.title}</h3> 
-                    <p>${note.text}</p>
-                    </li>
-                    `
-                    $('#noteList').append(temp_html);
+                    var noteItem = `
+                        <li class="list-group-item" data-note-id="${note._id}">
+                            <h3>${note.title}</h3> 
+                            <p>${note.text}</p>
+                            <button type="button" class="btn btn-primary edit-btn">Edit</button>
+                            <button type="button" class="btn btn-danger delete-btn">Delete</button>
+                        </li>`;
+                    $('#noteList').append(noteItem);
                 });
             }
         });
@@ -44,9 +44,60 @@ $(document).ready(function() {
             url: '/notes',
             type: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify({title: title, text: text}),
+            data: JSON.stringify({ title: title, text: text }),
             success: function() {
+                $('#noteTitle').val('');
                 $('#noteText').val('');
+                loadNotes();
+            }
+        });
+    }
+
+    // Edit note
+    $(document).on('click', '.edit-btn', function() {
+        var noteItem = $(this).closest('.list-group-item');
+        var noteId = noteItem.data('note-id');
+        var noteTitle = noteItem.find('h3').text();
+        var noteText = noteItem.find('p').text();
+
+        var newTitle = prompt('Enter new title:', noteTitle);
+        var newText = prompt('Enter new text:', noteText);
+
+        if (newTitle !== null && newText !== null) {
+            updateNote(noteId, newTitle, newText);
+        }
+    });
+
+    // Delete note
+    $(document).on('click', '.delete-btn', function() {
+        var noteItem = $(this).closest('.list-group-item');
+        var noteId = noteItem.data('note-id');
+        var confirmDelete = confirm('Are you sure you want to delete this note?');
+
+        if (confirmDelete) {
+            deleteNote(noteId);
+        }
+    });
+
+    // Function to update a note
+    function updateNote(id, title, text) {
+        $.ajax({
+            url: '/notes?id=' + id,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify({ title: title, text: text }),
+            success: function() {
+                loadNotes();
+            }
+        });
+    }
+
+    // Function to delete a note
+    function deleteNote(id) {
+        $.ajax({
+            url: '/notes?id=' + id,
+            type: 'DELETE',
+            success: function() {
                 loadNotes();
             }
         });
