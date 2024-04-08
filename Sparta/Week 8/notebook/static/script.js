db887185ad42fae283dc342c0ff5c38a7ecfaff6
Shiv Modi <modishiv210@gmail.com>
2024-04-08 08:08:12 +0530

    Week 8 Added

--- Sparta/Week 8/notebook/static/script.js
+++ Sparta/Week 8/notebook/static/script.js
@@ -54,19 +54,61 @@ $(document).ready(function() {
     }
 
     // Edit note
+    // $(document).on('click', '.edit-btn', function() {
+    //     var noteItem = $(this).closest('.list-group-item');
+    //     var noteId = noteItem.data('note-id');
+    //     var noteTitle = noteItem.find('h3').text();
+    //     var noteText = noteItem.find('p').text();
+
+    //     var newTitle = prompt('Enter new title:', noteTitle);
+    //     var newText = prompt('Enter new text:', noteText);
+
+    //     if (newTitle !== null && newText !== null) {
+    //         updateNote(noteId, newTitle, newText);
+    //     }
+    // });
+
     $(document).on('click', '.edit-btn', function() {
         var noteItem = $(this).closest('.list-group-item');
         var noteId = noteItem.data('note-id');
-        var noteTitle = noteItem.find('h3').text();
-        var noteText = noteItem.find('p').text();
-
-        var newTitle = prompt('Enter new title:', noteTitle);
-        var newText = prompt('Enter new text:', noteText);
-
-        if (newTitle !== null && newText !== null) {
+        var noteTitle = noteItem.find('h3');
+        var noteText = noteItem.find('p');
+    
+        // Convert the title and text into input fields
+        var newTitleInput = $('<input type="text" class="form-control">').val(noteTitle.text());
+        var newTextarea = $('<textarea class="form-control">').text(noteText.text());
+    
+        // Replace the title and text with input fields
+        noteTitle.replaceWith(newTitleInput);
+        noteText.replaceWith(newTextarea);
+    
+        // Focus on the new input for convenience
+        newTitleInput.focus();
+    
+        // Replace the edit button with a save button
+        $(this).removeClass('edit-btn').addClass('save-btn').text('Save');
+        
+        // newTitleInput.on('input', function() {
+        //     var inputText = $(this).val();
+        //     noteTitle.html('<strong>' + inputText + '</strong>');
+        // });
+    
+        // Attach click event handler to the new save button
+        $(document).on('click', '.save-btn', function() {
+            var newTitle = newTitleInput.val();
+            var newText = newTextarea.val();
+    
+            // Restore the title and text elements
+            newTitleInput.replaceWith(noteTitle.text(newTitle));
+            newTextarea.replaceWith(noteText.text(newText));
+    
+            // Replace the save button with the edit button
+            $(this).removeClass('save-btn').addClass('edit-btn').text('Edit');
+    
+            // Call the updateNote function if needed
             updateNote(noteId, newTitle, newText);
-        }
-    });
+        });
+    });    
 
     // Delete note
     $(document).on('click', '.delete-btn', function() {
